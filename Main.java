762b1cd43e75f6dda04b27a8e009c6baf8833491
nelsonklingenfuss <nelson.klingenfuss@univille.br>
2019-05-28 20:17:30 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+    Knapsack knapsack = new Knapsack(30);
+		BruteForceAlgorithm brute = new BruteForceAlgorithm();
+		brute.execute(knapsack);
+  }
+}
